c46a383ad1c213e6b34ffc23605a73d67b13427c
djouni <joaoeduardo411@gmail.com>
2024-05-14 23:52:50 -0300

    variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,29 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String meuNome = "JOAO EDUARDO";
+
+        System.out.println(meuNome);
+
+        double salarioMinimo = 2500;
+
+        System.out.println(salarioMinimo);
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.println(numeroCurto2);
+
+        int numero = 5;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14; // Constante
+
+        System.out.println(VALOR_DE_PI);
+
+
+
     }
 }
